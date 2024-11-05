fa63fdb033b7d66b42311ff2e34068b7655f98fa
king0jin <et0709@naver.com>
2024-11-05 15:06:17 +0900

    수정

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 
 app.set('port', process.env.PORT || 3000)
 
-app.get('/', (req, res) => {res.send('hello~ how are you?');})
+app.get('/', (req, res) => {res.send('hello~ how are you today??');})
 
 app.listen(app.get('port'), () => {
     console.log(app.get('port'), '번에서 대기 중 입니다.')
